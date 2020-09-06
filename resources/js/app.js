dafd68fb908decd8a6dd52f05f1e475746ac308d
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2020-09-07 02:43:09 +0300

    format

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -292,5 +292,5 @@ Vue.component(
 export const bus = new Vue();
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
 });
