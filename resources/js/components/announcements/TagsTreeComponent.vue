c0254715623a4a67b18bff5ab4700be40b6df7d2
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-04-12 00:33:26 +0200

    add search term

--- resources/js/components/announcements/TagsTreeComponent.vue
+++ resources/js/components/announcements/TagsTreeComponent.vue
@@ -2,7 +2,12 @@
   <nav class="panel is-shadowless">
     <div class="panel-block">
       <p class="control has-icons-left">
-        <input class="input is-small" type="text" placeholder="Search" />
+        <input
+          class="input is-small"
+          type="text"
+          placeholder="Search"
+          v-model="searchTerm"
+        />
         <span class="icon is-left">
           <i class="fas fa-search" aria-hidden="true"></i>
         </span>
@@ -80,60 +85,6 @@
         </div>
       </div>
     </div>
-    <!--  -->
-    <div class="panel-block tags-panel-block">
-      <div class="columns is-mobile">
-        <div class="column is-narrow">
-          <span class="icon is-clickable">
-            <i class="fas fa-caret-right"></i>
-          </span>
-        </div>
-        <div class="column is-narrow">
-          <span class="icon is-clickable">
-            <i class="far fa-square"></i>
-          </span>
-        </div>
-        <div class="column is-narrow">
-          <span class="is-unselectable">5o etos</span>
-        </div>
-      </div>
-    </div>
-    <!--  -->
-    <div class="panel-block tags-panel-block">
-      <div class="columns is-mobile">
-        <div class="column is-narrow">
-          <span class="icon is-clickable">
-            <i class="fas fa-caret-down"></i>
-          </span>
-        </div>
-        <div class="column is-narrow">
-          <span class="icon is-clickable">
-            <i class="far fa-check-square"></i>
-          </span>
-        </div>
-        <div class="column is-narrow">
-          <span class="is-unselectable">1o etos</span>
-        </div>
-      </div>
-    </div>
-    <!--  -->
-    <div class="panel-block tags-panel-block">
-      <div class="columns is-mobile">
-        <div class="column is-offset-1 is-narrow">
-          <span class="icon is-clickable">
-            <i class="fas fa-caret-down"></i>
-          </span>
-        </div>
-        <div class="column is-narrow">
-          <span class="icon is-clickable">
-            <i class="far fa-check-square"></i>
-          </span>
-        </div>
-        <div class="column is-narrow">
-          <span class="is-unselectable">2o etos</span>
-        </div>
-      </div>
-    </div>
   </nav>
 </template>
 
@@ -144,6 +95,11 @@ export default {
       type: Array,
       required: true,
     },
+  },
+  data: function () {
+    return {
+      searchTerm: "",
+    };
   },
 };
 </script>
