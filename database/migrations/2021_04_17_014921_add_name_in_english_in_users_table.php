5a95e4e36bc98e57c081f2bd28009d77a9768a7b
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-04-17 00:52:58 +0200

    add name in english

--- database/migrations/2021_04_17_014921_add_name_in_english_in_users_table.php
+++ database/migrations/2021_04_17_014921_add_name_in_english_in_users_table.php
@@ -14,7 +14,7 @@ class AddNameInEnglishInUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('name_eng')->after('name');
         });
     }
 
@@ -26,7 +26,7 @@ class AddNameInEnglishInUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('name_eng');
         });
     }
 }
