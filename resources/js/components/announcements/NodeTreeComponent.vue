d61ae56ae3b4ce4707275cc547de5829e26bb8ca
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-04-16 23:57:23 +0200

    test

--- resources/js/components/announcements/NodeTreeComponent.vue
+++ resources/js/components/announcements/NodeTreeComponent.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <p class="is-size-5">{{ node.title }}</p>
+    <div class="panel-block tags-panel-block">
+      <div class="columns is-mobile">
+        <div class="column is-offset-0 is-narrow">
+          <tag-caret></tag-caret>
+        </div>
+        <div class="column is-narrow">
+          <tag-click></tag-click>
+        </div>
+        <div class="column is-narrow">
+          <span class="is-unselectable">{{ node.title }}</span>
+        </div>
+      </div>
+    </div>
 
     <ul v-if="node.children && node.children.length">
       <node v-for="child in node.children" :node="child" :key="node.id"></node>
