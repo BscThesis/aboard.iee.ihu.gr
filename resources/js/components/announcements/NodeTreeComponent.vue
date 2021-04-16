ec92e80eaeca93e6a40010aea59048a6a0313482
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-04-16 23:47:33 +0200

    asdfa

--- resources/js/components/announcements/NodeTreeComponent.vue
+++ resources/js/components/announcements/NodeTreeComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p class="is-size-5">{{ node[0].name }}</p>
+    <p class="is-size-5">{{ node[0] }}</p>
 
     <ul v-if="node.children && node.children.length">
       <node v-for="child in node.children" :node="child" :key="node.id"></node>
