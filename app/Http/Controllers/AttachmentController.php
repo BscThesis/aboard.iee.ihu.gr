05cf10850fda74145ce2c9240edc1dff5750f813
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-04-24 01:20:12 +0200

    remove header

--- app/Http/Controllers/AttachmentController.php
+++ app/Http/Controllers/AttachmentController.php
@@ -57,6 +57,7 @@ class AttachmentController extends Controller
                     // 'Content-Disposition' => "attachment; filename=" . $attachment->filename,
                     'Content-Type' => $attachment->mime_type,
                     'Content-Length' => $attachment->filesize,
+
                 ]);
         } else {
             return response()->json([
