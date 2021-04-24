e1e5d731a61c2257bd436c6cdb07e30c45c2bcf9
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-04-25 01:36:46 +0200

    test more

--- app/Announcement.php
+++ app/Announcement.php
@@ -74,6 +74,8 @@ class Announcement extends Model implements Feedable
         // return $this->whereHas('tags', function (Builder $query) {
         //     $query->where('is_public', '=', 1);
         // });
-        return $this->tags()->exists();
+        return $this->tags()->whereHas('tags', function (Builder $query) {
+            $query->where('is_public', '=', 1);
+        });
     }
 }
