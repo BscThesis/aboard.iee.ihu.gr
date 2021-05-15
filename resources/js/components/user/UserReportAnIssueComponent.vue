1e12df8f087834a2819b7723deb9901329ce8c44
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-05-15 21:56:44 +0200

    first take at issue reporting

--- resources/js/components/user/UserReportAnIssueComponent.vue
+++ resources/js/components/user/UserReportAnIssueComponent.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="block">
-    <h1 class="has-text-danger">Work in progress</h1>
+    <div class="field">
+      <label class="label">Τίτλος</label>
+      <div class="control">
+        <input
+          class="input"
+          type="text"
+          placeholder="Τίτλος"
+          v-model="issue.title"
+        />
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Κείμενο</label>
+      <div class="control">
+        <textarea
+          class="textarea"
+          placeholder="Κείμενο"
+          v-model="issue.title"
+        ></textarea>
+      </div>
+    </div>
+
+    <div class="field">
+      <div class="control">
+        <button class="button is-link">Υποβολή</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      issue: {},
+    };
+  },
+};
 </script>
 
 <style>
