00a386bbfae8e5949d7b31bd70db11b5c7ed6e9c
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-05-22 11:54:37 +0200

    a

--- resources/js/components/user/LoginContainerComponent.vue
+++ resources/js/components/user/LoginContainerComponent.vue
@@ -101,7 +101,7 @@ export default {
           })
           .then(function (response) {
             if (response.status == 200) {
-              console.log("response: " + response.data);
+              console.log("response: " + JSON.stringify(response.data));
               toast({
                 message: "Συνδεθήκατε επιτυχώς",
                 type: "is-success",
