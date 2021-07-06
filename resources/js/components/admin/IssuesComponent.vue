8ce906d4344d09a6601af8349d015e3758f2724c
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-07-06 21:08:38 +0200

    view isses comp

--- resources/js/components/admin/IssuesComponent.vue
+++ resources/js/components/admin/IssuesComponent.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="block"></div>
+  <div class="block">
+    <table
+      v-if="issues.length >= 0"
+      class="table is-fullwidth is-bordered is-hoverable"
+    >
+      <thead>
+        <tr>
+          <th class="has-text-centered">Τίτλος</th>
+          <th class="has-text-centered">Περιγραφή</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="issue in issues" v-bind:key="issue.id">
+          <td class="has-text-centered" v-bind:title="issue.title">
+            {{ issue.title }}
+          </td>
+          <td class="has-text-centered">
+            {{ issue.body }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
