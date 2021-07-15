d25aca58eeffdecfc067a65ef62f310b4bfa0566
Nikolaos Christos Nikolaidis <storm1885@gmail.com>
2021-07-15 23:29:04 +0200

    add exception to middleware

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api')->except('login');
+        $this->middleware('auth:api')->except(['login', 'refresh']);
     }
 
     /**
