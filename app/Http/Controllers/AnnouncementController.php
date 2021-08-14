ea6f1eb4c82d87c82866cc63708a86a11a273d62
Nikolaos Christos Nikolaidis <6990195+nickcn@users.noreply.github.com>
2021-08-14 19:30:43 +0200

    log

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -42,7 +42,7 @@ class AnnouncementController extends Controller
     {
         $local_ip = $request->session()->get('local_ip', 0);
 
-        \Log::info(var_dump($request));
+        \Log::info(var_dump($request->all()));
 
         if ($local_ip == 1 or Auth::guard('api')->check()) {
             $announcements = Announcement::orderBy('updated_at', 'desc')->paginate(10);
