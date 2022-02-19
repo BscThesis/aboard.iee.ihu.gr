3f45e0d7cb969dca46e435921206ccca0162afb4
pant377 <pantpap15@gmail.com>
2022-02-19 21:06:16 +0200

    changed store and update functions in AnnouncementController

--- app/Http/Controllers/Announcement/AnnouncementController.php
+++ app/Http/Controllers/Announcement/AnnouncementController.php
@@ -119,10 +119,12 @@ class AnnouncementController extends Controller
                 $tags = array();
                 foreach ($request->input('tags') as $id) {
                     $tag = Tag::findOrFail($id);
-                    while (!is_null($tag->parent_id)) {
-                        //if (!is_null($tag->parent_id)) {}
-                        array_push($tags, $tag->id);
-                        $tag = Tag::findOrFail($tag->parent_id);
+                    $parent = $tag->parent_id;
+                    while (!is_null($parent)) {
+                        // if (!is_null($tag->parent_id)) {}
+                            array_push($tags, $tag->id);
+                            $parent = $tag->parent_id;
+                            $tag = Tag::findOrFail($parent);
                     }
                 }
                 $announcement->tags()->sync($tags);
@@ -205,11 +207,12 @@ class AnnouncementController extends Controller
                 $tags = array();
                 foreach ($request->input('tags') as $id) {
                     $tag = Tag::findOrFail($id);
-
-                    while (!is_null($tag->parent_id)) {
+                    $parent = $tag->parent_id;
+                    while (!is_null($parent)) {
                         // if (!is_null($tag->parent_id)) {}
-                        array_push($tags, $tag->id);
-                        $tag = Tag::findOrFail($tag->parent_id);
+                            array_push($tags, $tag->id);
+                            $parent = $tag->parent_id;
+                            $tag = Tag::findOrFail($parent);
                     }
                 }
 
